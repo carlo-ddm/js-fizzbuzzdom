7fbd8781e6098682b537f963d8d99ee2d8b71e26
carlo-ddm <carloduminuco90@gmail.com>
2022-04-10 17:06:43 +0200

    else if dei multipli di 3 e 5... correggere

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 console.log('test');
 
 const container = document.querySelector ('.container');
-const limit = 70;
+const limit = 100;
 
 for (let i = 1; i <= limit; i++) {
   const box = document.createElement ('div');
   box.className = 'box';
+  box.classList.add ('blue');
   box.append(i);
   console.log(box);
 
-  container.append(box)
+  container.append(box);
+  
+  if(!(i % 3)){
+    box.classList.add('fizz');
+    box.innerHTML = 'Fizz';
+    console.log(box);
+  } else if (!(i % 5)) {
+    box.classList.add('buzz');
+    box.innerHTML = 'Buzz';
+    console.log(box);
+  } else if (!(i % 3) && !(i % 5)){
+    box.classList.add('fizz-buzz');
+    box.innerHTML = 'FizzBuzz';
+    console.log(box);
+  }
 }
