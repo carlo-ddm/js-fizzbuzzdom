44d921f455fc93092dfb46933755f725266507c2
carlo-ddm <carloduminuco90@gmail.com>
2022-04-10 16:28:57 +0200

    sistemazione errori di conteggio / colore font in css

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ console.log('test');
 const container = document.querySelector ('.container');
 const limit = 70;
 
-for (let i = 0; i < 70; i++) {
+for (let i = 1; i <= limit; i++) {
   const box = document.createElement ('div');
   box.className = 'box';
   box.append(i);
   console.log(box);
+
+  container.append(box)
 }
